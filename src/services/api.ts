5f73f90f7f0c97fa304952f955685f8e71d6bd98
sanjaysr77 <sanjaysrbangalore@gmail.com>
2026-01-05 06:09:41 +0530

    Everything before using Tanstack

---
+++ src/services/api.ts
@@ -0,0 +1,9 @@
+import axios from "axios"
+import type { Todo } from "../types/todo"
+
+const BASE_URL = "http://localhost:8080"
+const axiosInstance = axios.create({ baseURL: BASE_URL })
+
+export const getTodoIds = async () => {
+    return (await axiosInstance.get<Todo[]>('todos')).data.map((todo) => todo.id);
+}
